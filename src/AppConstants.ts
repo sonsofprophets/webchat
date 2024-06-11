6a226b7101a94e4754a8b3f3e63266a7e2b5e027
Ricky Brown <ricky@sonsofprophets.co>
2024-06-11 15:08:54 -0400

    set keys

--- src/AppConstants.ts
+++ src/AppConstants.ts
@@ -1,7 +1,14 @@
 // Head over to the Credentials section in your app on CometChat Dashboard and where you can find your unique App ID, Region and Auth Key. 😊
 
 export const AppConstants = {
-  APP_ID: "APP_ID",
-  REGION: "REGION",
-  AUTH_KEY: "AUTH_KEY",
+  APP_ID: "253640f82084b114",
+  REGION: "us",
+  AUTH_KEY: "01b3bc5c9041a4168b36686f663331331587861f",
 };
+
+
+// export const AppConstants = {
+//   APP_ID: process.env.APP_ID,
+//   REGION: process.env.REGION,
+//   AUTH_KEY: process.env.AUTH_KEY,
+// };
